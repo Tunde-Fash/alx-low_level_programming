413339df68bb1c69f6ca71dd9cbd56dcc2c789f3
Tudne-Fash <tundefashicy@gmail.com>
2023-09-07 15:18:11 -0700

    c5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message with proper
+ *		grammar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
