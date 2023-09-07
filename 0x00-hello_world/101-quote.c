283edf4b2cd4477a7900167a43c9f4a1b90e1742
Tudne-Fash <tundefashicy@gmail.com>
2023-09-07 15:36:03 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints a message to the standard error.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,"
+	"2015-10-19\n");
+	return (1);
+}
