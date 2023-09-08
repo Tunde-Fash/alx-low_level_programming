6cb9dbdf1ba092d514d5be90057060318bb44925
Tudne-Fash <tundefashicy@gmail.com>
2023-09-08 14:24:51 -0700

    var4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Ths program prints d lowercase alphabet, excluding 'q' and 'e',
+ * followed by a new line using the putchar function.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	if (letter != 'e' && letter != 'q')
+	{
+		putchar(letter);
+	}
+	letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
