be94bc0efe96b7772a9f441eeeb2ccced0ec1a6c
Tudne-Fash <tundefashicy@gmail.com>
2023-09-08 14:42:47 -0700

    var7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program prints the lowercase alphabet in reverse,
+ * followed by a new line, using only two putchar calls.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+	putchar(letter);
+	letter--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
