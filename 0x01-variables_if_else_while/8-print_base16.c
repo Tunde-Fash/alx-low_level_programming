caada40e99be0f19f1478fe5da1565de0dcb2fc4
Tudne-Fash <tundefashicy@gmail.com>
2023-09-08 14:45:02 -0700

    var8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program prints all hexadecimal numbers in lowercase,
+ * followed by a new line, using only three putchar calls.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+	putchar(digit);
+	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+	{
+	putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
