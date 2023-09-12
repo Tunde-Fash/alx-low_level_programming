0d960a102aa67d4d1d6c64806a23ac430f06bce4
Tudne-Fash <tundefashicy@gmail.com>
2023-09-12 13:50:56 -0700

    fun1f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ */
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -11,17 +14,3 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
-
-/**
- * main - Entry point of the program.
- *
- * Description: This program prints the lowercase alphabet followed by a new line.
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
