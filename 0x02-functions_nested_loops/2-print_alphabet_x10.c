50368b74f3538bc454ba91f017ff8654b250561a
Tudne-Fash <tundefashicy@gmail.com>
2023-09-12 15:15:36 -0700

    fun2c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,37 +1,32 @@
-#include <unistd.h>
-
-// Function prototype for _putchar (custom output function)
-int _putchar(char c);
-
-// Function prototype for print_alphabet_x10
-void print_alphabet_x10(void);
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line.
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, 1. On error, -1.
  */
-void print_alphabet_x10(void)
-{
-	char letter = 'a';
-
-	for (int i = 0; i < 10; i++) {
-		while (letter <= 'z') {
-			_putchar(letter); // Use your custom _putchar function here.
-			letter++;
-		}
-		letter = 'a';
-		_putchar('\n'); // Use your custom _putchar function here.
-	}
-}
-
-// Custom _putchar function implementation (replace this with your own)
 int _putchar(char c)
 {
 	return write(1, &c, 1);
 }
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototypes for _putchar and print_alphabet functions */
-
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
-
