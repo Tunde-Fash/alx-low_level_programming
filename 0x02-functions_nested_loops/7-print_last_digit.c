37e80f094c468dc3c429cb4736d8103671938873
Tudne-Fash <tundefashicy@gmail.com>
2023-09-12 16:20:16 -0700

    fun7b

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = -n; // Make it positive temporarily
-
+	{
+		_putchar('-');
+		n = -n;
+	}
 	last_digit = n % 10;
 	_putchar(last_digit + '0');
 
-	if (n < 0)
-		last_digit = -last_digit; // If n was negative, make last_digit negative
-
 	return (last_digit);
 }
+
