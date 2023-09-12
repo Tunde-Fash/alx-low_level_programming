999f353ced4a56149fcdb438c6959c22fb67f0a3
Tudne-Fash <tundefashicy@gmail.com>
2023-09-12 16:06:48 -0700

    fun6

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for _putchar function */
-int _putchar(char c);
-
-/* Prototype for print_sign function */
-int print_sign(int n);
+int _abs(int n);
 
 #endif /* MAIN_H */
