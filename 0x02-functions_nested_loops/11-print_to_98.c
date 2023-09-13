d3f38055674036463fe57c0844b3937f826f026d
Tudne-Fash <tundefashicy@gmail.com>
2023-09-13 12:30:16 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/* Custom putchar function (you can implement this elsewhere) */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
+/* Function to print natural numbers from n to 98 */
+void print_to_98(int n)
+{
+	/* Loop from n to 98 */
+	while (n != 98)
+	{
+		/* Print the current number */
+		printf("%d, ", n);
+
+		/* Update n based on whether it's greater or smaller than 98 */
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	}
+
+	/* Print the final number (98) followed by a newline */
+	printf("98\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,7 @@ void times_table(void);
 int add(int num1, int num2);
 int _putchar(char c);
 
+void print_to_98(int n);
+int _putchar(char c);
+
 #endif /* MAIN_H */
