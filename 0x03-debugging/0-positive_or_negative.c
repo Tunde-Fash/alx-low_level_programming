96aa0cb0832d263a1a0b41e26cd46fc037fdba91
Tudne-Fash <tundefashicy@gmail.com>
2023-09-14 13:20:19 -0700

    bug1

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program generates a random number, assigns it to the
+ * variable 'n', and prints whether 'n' is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        printf("%d is ", n);
+
+        if (n > 0)
+        {
+        printf("positive");
+        }
+        else if (n < 0)
+        {
+        printf("negative");
+        }
+        else
+        {
+        printf("zero");
+        }
+
+        printf("\n");
+
+        return (0);
+}
