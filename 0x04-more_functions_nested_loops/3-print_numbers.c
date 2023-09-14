1bac7cd1c4aa309ce316676fc979f1a213174278
Tudne-Fash <tundefashicy@gmail.com>
2023-09-14 15:07:22 -0700

    mfun4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,29 +1,18 @@
 #include "main.h"
-#include <unistd.h> // Include the unistd.h header for the write function
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- *
- * Return: On success, 1. On error, -1, and errno is set appropriately.
+ * print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
  */
-/* Define the _putchar function (assuming it's already implemented) */
-int _putchar(char c);
-{
-    return write(1, &c, 1);
-}
-
-
-/**
- * print_numbers - Prints numbers from 0 to 9 followed by a new line
- */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char digit;
 
 	for (digit = '0'; digit <= '9'; digit++)
 	{
-		_putchar(digit);
+		if (digit != '2' && digit != '4')
+		{
+			_putchar(digit);
+		}
 	}
 	_putchar('\n');
 }
