f3d2aa77bb4a403770e62f7f7bf8dd8631c590a3
Tudne-Fash <tundefashicy@gmail.com>
2023-09-18 07:21:54 -0700

    f

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,18 @@
  */
 void print_array(int *a, int n)
 {
-	if (a == NULL)  /* Check for NULL pointer (0) */
+	if (a == 0)  /* Check for NULL pointer (0) */
 		return;
 
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]); /* Print the current element */
 
 		if (i < n - 1)
 			printf(", "); /* Print comma and space unless it's the last element */
-    }
+	}
 
 	printf("\n"); /* Print a newline character at the end */
 }
