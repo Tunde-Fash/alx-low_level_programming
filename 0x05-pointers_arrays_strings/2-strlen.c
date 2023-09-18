8db26af759ea231763b7390b70a5de216baa1c2a
Tudne-Fash <tundefashicy@gmail.com>
2023-09-18 06:17:15 -0700

    point1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -13,7 +13,35 @@ int main(void)
 	char *str = "Hello, World!";
 	int length = _strlen(str);
 
-	_putchar('The length of the string is: ');
+	_putchar('T');
+	_putchar('h');
+	_putchar('e');
+	_putchar(' ');
+	_putchar('l');
+	_putchar('e');
+	_putchar('n');
+	_putchar('g');
+	_putchar('t');
+	_putchar('h');
+	_putchar(' ');
+	_putchar('o');
+	_putchar('f');
+	_putchar(' ');
+	_putchar('t');
+	_putchar('h');
+	_putchar('e');
+	_putchar(' ');
+	_putchar('s');
+	_putchar('t');
+	_putchar('r');
+	_putchar('i');
+	_putchar('n');
+	_putchar('g');
+	_putchar(' ');
+	_putchar('i');
+	_putchar('s');
+	_putchar(':');
+	_putchar(' ');
 	_putchar(length + '0');  /* Convert integer to character */
 	_putchar('\n');
 
