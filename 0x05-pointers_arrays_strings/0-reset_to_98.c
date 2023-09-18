b6d444fc75f0dcebf048bafa2e0ca3fa99302a5c
Tudne-Fash <tundefashicy@gmail.com>
2023-09-18 06:02:12 -0700

    point0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98.
+ * @n: A pointer to an integer.
+ */
+void reset_to_98(int *n)
+{
+	/* Check if n is not NULL before dereferencing */
+	if (n != NULL)
+	{
+		*n = 98;
+	}
+}
