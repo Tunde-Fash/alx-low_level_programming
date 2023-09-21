e541e305ebae65ec938d3cab5474bfe366c886ad
Tudne-Fash <tundefashicy@gmail.com>
2023-09-21 12:48:25 -0700

    6a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,41 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalize all words in a string
- * @s: The input string
- *
+ * is_separator - Checks if a character is a word separator.
+ * @c: The character to check.
+ * Return: 1 if it's a separator, 0 otherwise.
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i]; i++)
+	{
+		if (c == separators[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words in a string.
+ * @s: The input string.
  * Return: Pointer to the modified string.
  */
 char *cap_string(char *s)
 {
-	int i, cap_next = 1;
-	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (s == NULL || s[0] == '\0')
+		return (s);
+
+	for (i = 0; s[i]; i++)
 	{
-		if (cap_next && s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			s[i] = s[i] - 'a' + 'A';
-			cap_next = 0;
-		}
-
-		if (strchr(separators, s[i]) != NULL)
-		{
-			cap_next = 1;
+			if (i == 0 || is_separator(s[i - 1]))
+				s[i] -= 32; /* Convert to uppercase */
 		}
 	}
 
