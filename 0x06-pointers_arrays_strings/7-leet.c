6bee3402a3eb50ea9776134b7fcdf84683c97238
Tudne-Fash <tundefashicy@gmail.com>
2023-09-21 13:29:21 -0700

    7b

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,31 @@
 #include "main.h"
 
 /**
- * leet - Encode a string into "1337" (leet speak).
+ * leet - Encodes a string into "1337".
  * @str: The input string to be encoded.
  *
- * Return: A pointer to the modified string.
+ * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
 {
-	int i;
-	char leet_map[256] = {0};  /* Initialize a mapping array to all zeros. */
+	char *ptr = str;
+	char leet_map[256] = {0}; /* Initialize the leet_map with zeros */
 
-	/* Initialize the mapping for lowercase letters. */
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == 'a' || i == 'e' || i == 'o' || i == 't' || i == 'l')
-			leet_map[i] = '4';
-		else
-			leet_map[i] = i;
-	}
+	/* Initialize the leet_map with the required mappings */
+	leet_map['a'] = leet_map['A'] = '4';
+	leet_map['e'] = leet_map['E'] = '3';
+	leet_map['o'] = leet_map['O'] = '0';
+	leet_map['t'] = leet_map['T'] = '7';
+	leet_map['l'] = leet_map['L'] = '1';
 
-    /* Initialize the mapping for uppercase letters. */
-	for (i = 'A'; i <= 'Z'; i++)
+	/* Loop through the input string and replace characters as needed */
+	while (*ptr)
 	{
-		if (i == 'A' || i == 'E' || i == 'O' || i == 'T' || i == 'L')
-			leet_map[i] = '4';
-		else
-			leet_map[i] = i;
-	}
-
-	/* Loop through the input string and apply the mapping. */
-	for (i = 0; str[i] != '\0'; i++)
-	{
-	str[i] = leet_map[str[i]];
+		if (leet_map[(unsigned char)*ptr])
+		{
+			*ptr = leet_map[(unsigned char)*ptr];
+		}
+		ptr++;
 	}
 
 	return (str);
