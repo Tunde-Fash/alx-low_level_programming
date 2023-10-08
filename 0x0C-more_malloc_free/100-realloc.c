3a4d5f3cb4ed017041b667449ac845b1f633a178
Tudne-Fash <tundefashicy@gmail.com>
2023-10-08 12:06:09 -0700

    100d

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _realloc - Reallocate a memory block using malloc and free.
- * @ptr: Pointer to the memory previously allocated with malloc.
- * @old_size: Size in bytes of the allocated space for ptr.
- * @new_size: New size in bytes of the new memory block.
+ * _realloc - Reallocates a memory block.
+ * @ptr: Pointer to the previously allocated memory block (or NULL).
+ * @old_size: Size of the old memory block in bytes.
+ * @new_size: New size of the memory block in bytes.
  *
  * Return: Pointer to the newly allocated memory block.
  */
@@ -13,38 +12,48 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
     void *new_ptr;
 
-    if (new_size == old_size)
-        return ptr;
+    /* Handle special cases */
+
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
     if (ptr == NULL)
     {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-            return NULL;
-        return new_ptr;
+        return (malloc(new_size));
     }
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
+    /* Allocate a new memory block */
 
     new_ptr = malloc(new_size);
     if (new_ptr == NULL)
-        return NULL;
+    {
+        return (NULL);
+    }
 
-    /* Copy the minimum of old_size and new_size bytes */
-    if (old_size < new_size)
-        new_size = old_size;
+    /* Copy data from the old block to the new block */
 
-    char *source = ptr;
-    char *dest = new_ptr;
+    if (new_size <= old_size)
+    {
+        for (unsigned int i = 0; i < new_size; i++)
+        {
+            *((char *)new_ptr + i) = *((char *)ptr + i);
+        }
+    }
+    else
+    {
+        for (unsigned int i = 0; i < old_size; i++)
+        {
+            *((char *)new_ptr + i) = *((char *)ptr + i);
+        }
+    }
 
-    while (new_size--)
-        *dest++ = *source++;
+    /* Free the old memory block */
 
-    free(ptr); /* Free the old memory block */
-    return new_ptr;
+    free(ptr);
+
+    return (new_ptr);
 }
 
