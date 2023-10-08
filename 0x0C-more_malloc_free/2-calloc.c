3f075c6ffc3a04ff642aa41aa2042c426e6184b2
Tudne-Fash <tundefashicy@gmail.com>
2023-10-08 11:49:34 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,26 +5,26 @@
  * @nmemb: Number of elements
  * @size: Size of each element
  *
- * Return: Pointer to the allocated memory, NULL on failure or if nmemb/size is 0
+ * Return: Pointer to the allocated memory, NULL on failure or if nmemb/size
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int i;
-    unsigned int total_size;
+	void *ptr;
+	unsigned int i;
+	unsigned int total_size;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+	return (NULL);
 
-    total_size = nmemb * size;
-    ptr = malloc(total_size);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
 
-    if (ptr == NULL)
-        return (NULL);
+	if (ptr == NULL)
+	return (NULL);
 
-    /* Initialize allocated memory to zero */
-    for (i = 0; i < total_size; i++)
-        *((char *)ptr + i) = 0;
+	/* Initialize allocated memory to zero */
+	for (i = 0; i < total_size; i++)
+	*((char *)ptr + i) = 0;
 
-    return (ptr);
+	return (ptr);
 }
