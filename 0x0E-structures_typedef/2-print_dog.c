643690bfe958a2a47c61df582ccfc3386ab0628a
Tudne-Fash <tundefashicy@gmail.com>
2023-10-10 09:58:54 -0700

    2a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,45 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dog
-{
-	char *name;
-	char *breed;
-	int age;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
 };
 
-void print_dog(struct dog *d)
-{
-	if (d == NULL)
-	{
-	return;
-    }
-	if (d->name == NULL)
-	{
-	printf("Name: (nil)\n");
-	}
-	else
-	{
-	printf("Name: %s\n", d->name);
-	}
-	if (d->breed == NULL)
-	{
-	printf("Breed: (nil)\n");
-	} 
-	else
-	{
-	printf("Breed: %s\n", d->breed);
-	}
-	printf("Age: %d\n", d->age);
-}
+/**
+ * print_dog - Prints information about a dog
+ * @d: Pointer to a struct dog
+ *
+ * This function prints the name, age, and owner of a dog. If any of these
+ * fields are NULL, it prints "(nil)" for that field. If the input pointer
+ * d is NULL, this function does nothing.
+ */
+void print_dog(struct dog *d);
 
-int main()
-{
-	struct dog my_dog;
-	my_dog.name = "Buddy";
-	my_dog.breed = NULL;
-	my_dog.age = 3;
+int main() {
+    struct dog my_dog;
+    my_dog.name = "Buddy";
+    my_dog.age = 3.5;
+    my_dog.owner = "John Doe";
 
-	print_dog(&my_dog);
-	return 0;
+    print_dog(&my_dog);
+
+    return 0;
 }
