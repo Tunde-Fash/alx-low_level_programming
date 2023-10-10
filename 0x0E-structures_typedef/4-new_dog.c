afae736b4bc05088b36d5db7eb34f1eadd74e917
Tudne-Fash <tundefashicy@gmail.com>
2023-10-10 10:30:51 -0700

    4b

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,37 +13,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog_ptr;
-    char *name_copy;
-    char *owner_copy;
+	dog_t *new_dog_ptr;
+	char *name_copy;
+	char *owner_copy;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+	return (NULL);
 
-    new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL)
-        return (NULL);
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+	return (NULL);
 
-    name_copy = malloc(strlen(name) + 1);
-    if (name_copy == NULL)
-    {
-        free(new_dog_ptr);
-        return (NULL);
-    }
-    strcpy(name_copy, name);
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+	free(new_dog_ptr);
+	return (NULL);
+	}
+	strcpy(name_copy, name);
 
-    owner_copy = malloc(strlen(owner) + 1);
-    if (owner_copy == NULL)
-    {
-        free(new_dog_ptr);
-        free(name_copy);
-        return (NULL);
-    }
-    strcpy(owner_copy, owner);
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+	free(new_dog_ptr);
+	free(name_copy);
+	return (NULL);
+	}
+	strcpy(owner_copy, owner);
 
-    new_dog_ptr->name = name_copy;
-    new_dog_ptr->age = age;
-    new_dog_ptr->owner = owner_copy;
+	new_dog_ptr->name = name_copy;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner_copy;
 
-    return (new_dog_ptr);
+	return (new_dog_ptr);
 }
