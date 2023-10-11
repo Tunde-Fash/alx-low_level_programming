1d0b320f1049e8b71f0a5e2e462a901645884800
Tudne-Fash <tundefashicy@gmail.com>
2023-10-11 06:58:35 -0700

    0b

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,37 +1,19 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - Prints a name using a function pointer
  * @name: The name to be printed
- * @f: A function pointer to a function that prints the name
+ * @f: A function pointer to a function that prints a string
+ *
+ * Description: This function takes a name and a function pointer as arguments.
+ * It then uses the function pointer to print the name.
  */
 void print_name(char *name, void (*f)(char *))
 {
     if (name != NULL && f != NULL)
-        f(name);
-}
-
-/**
- * print_name_with_putchar - A function to print a name using _putchar
- * @name: The name to be printed
- */
-void print_name_with_putchar(char *name)
-{
-    while (*name)
     {
-        _putchar(*name);
-        name++;
+        f(name);
     }
-    _putchar('\n');
-}
-
-/**
- * _putchar - A custom putchar function
- * @c: The character to be printed
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
 }
 
