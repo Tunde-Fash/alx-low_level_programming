9fc8c02fb9177ce253678ffe55aa8301d99a4f80
Tudne-Fash <tundefashicy@gmail.com>
2023-10-11 08:46:42 -0700

    100a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
-void print_opcodes(char *func_ptr, int num_bytes) {
+/**
+ * print_error_and_exit - Prints an error message and exits with the specified status code.
+ * @message: The error message to be printed.
+ * @status: The exit status code.
+ */
+void print_error_and_exit(const char *message, int status) {
+    fprintf(stderr, "%s\n", message);
+    exit(status);
+}
+
+/**
+ * print_opcodes - Prints the opcodes of the main function in hexadecimal format.
+ * @num_bytes: The number of bytes to print.
+ */
+void print_opcodes(int num_bytes) {
+    unsigned char *main_ptr = (unsigned char *)print_opcodes;
     for (int i = 0; i < num_bytes; i++) {
-        printf("%02x", func_ptr[i] & 0xFF);
+        printf("%02x", main_ptr[i]);
     }
     printf("\n");
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Error\n");
-        return 1;
+        print_error_and_exit("Error: Invalid number of arguments", 1);
     }
 
     int num_bytes = atoi(argv[1]);
 
     if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
+        print_error_and_exit("Error: Number of bytes cannot be negative", 2);
     }
 
-    print_opcodes((char *)main, num_bytes);
+    print_opcodes(num_bytes);
 
     return 0;
 }
+
