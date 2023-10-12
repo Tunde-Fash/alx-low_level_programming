4fa8156988fceac0ca1658e7652ec53a281c13c1
Tudne-Fash <tundefashicy@gmail.com>
2023-10-12 12:02:39 -0700

    2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,14 +5,15 @@
 
 typedef struct print_format
 {
-	char *format;
-	void (*func)(va_list);
+        char *format;
+        void (*func)(va_list);
 }
-	print_format_t;
+        print_format_t;
 
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
