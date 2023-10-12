94e4d667b3ca632c8381e8e344ec650526d8bfd3
Tudne-Fash <tundefashicy@gmail.com>
2023-10-12 12:35:33 -0700

    0c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,10 +2,10 @@
 
 /**
  * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of arguments
- * @...: The parameters to be summed
+ * @n: The number of parameters.
+ * @...: The parameters to be summed.
  *
- * Return: The sum of all parameters, or 0 if n is 0.
+ * Return: The sum of all the parameters, or 0 if n is 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -14,15 +14,17 @@ int sum_them_all(const unsigned int n, ...)
     unsigned int i;
 
     if (n == 0)
-        return 0;
+        return (0);
 
     va_start(args, n);
+
     for (i = 0; i < n; i++)
     {
         sum += va_arg(args, int);
     }
+
     va_end(args);
 
-    return sum;
+    return (sum);
 }
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,6 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
