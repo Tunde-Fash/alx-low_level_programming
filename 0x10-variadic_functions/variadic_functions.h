5726a28829f037a70d73c3a35d12d1f51d92de0c
Tudne-Fash <tundefashicy@gmail.com>
2023-10-12 11:50:21 -0700

    0a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,18 @@
 
 #include <stdarg.h>
 
+/**
+ * struct format_t - Struct format_t
+ *
+ * @format: The format specifier
+ * @f: The function associated with the format specifier
+ */
+typedef struct format_t
+{
+	char *format;
+	void (*f)(va_list);
+} format_t;
+
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
