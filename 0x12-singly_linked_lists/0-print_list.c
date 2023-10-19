75d973d4d8be0554b3a69c535c842eca5dcc06b4
Tudne-Fash <tundefashicy@gmail.com>
2023-10-19 10:43:46 -0700

    0a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -24,3 +24,37 @@ size_t print_list(const list_t *h)
 	return (count);
 }
 
+/**
+ * main - Entry point of the program, demonstrates the usage of print_list.
+ *
+ * This function creates a sample linked list and uses the print_list function
+ * to print its elements and count the number of nodes in the list.
+ *
+ * Return: Always returns 0 to indicate successful completion.
+ */
+int main(void)
+{
+    list_t *head = NULL;
+
+    /* Create a sample linked list */
+    list_t node1, node2, node3;
+    node1.str = "Hello";
+    node1.len = 5;
+    node1.next = &node2;
+
+    node2.str = "World";
+    node2.len = 5;
+    node2.next = &node3;
+
+    node3.str = "!";
+    node3.len = 1;
+    node3.next = NULL;
+
+    head = &node1;
+
+    size_t count = print_list(head);
+    printf("Number of nodes: %zu\n");
+
+    return (0);
+}
+
