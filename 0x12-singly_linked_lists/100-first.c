8957511b566e6ca84a6b0d40b7c39c93c10a90ec
Tudne-Fash <tundefashicy@gmail.com>
2023-10-19 11:18:38 -0700

    100a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,11 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * _putchar - writes a character to stdout
- * @c: The character to print
- *
- * Return: On success, 1; on error, -1.
+ * pre_main_message - Prints a message before the main function is executed.
  */
-int _putchar(char c)
+void pre_main_message(void)
 {
-	return write(1, &c, 1);
-}
-
-/**
- * print_before_main - prints the specified text before the main function is executed
- */
-void print_before_main(void)
-{
-	char *text = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	int i;
-
-	for (i = 0; text[i]; i++)
-		_putchar(text[i]);
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,8 +27,7 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
-int _putchar(char c);
-void print_before_main(void);
+void pre_main_message(void);
 
 #endif /* LISTS_H */
 
