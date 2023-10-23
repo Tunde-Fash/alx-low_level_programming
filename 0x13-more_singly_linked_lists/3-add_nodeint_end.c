68e7b24c020118138d25f1b10dc0243099ad105f
Tudne-Fash <tundefashicy@gmail.com>
2023-10-23 13:31:58 -0700

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,29 +10,28 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node, *current;
+	listint_t *new_node, *current;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+	*head = new_node;
+	return (new_node);
+	}
 
-    current = *head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-    current->next = new_node;
+	current = *head;
+	while (current->next != NULL)
+	{
+	current = current->next;
+	}
+	current->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
-
