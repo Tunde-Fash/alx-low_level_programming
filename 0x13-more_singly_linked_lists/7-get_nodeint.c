a15a43b3f1aad93383d026d204d92424648c5972
Tudne-Fash <tundefashicy@gmail.com>
2023-10-24 03:59:50 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,17 +8,17 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    listint_t *current = head;
+	unsigned int i = 0;
+	listint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (i == index)
-            return current;
-        i++;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+	if (i == index)
+	return (current);
+	i++;
+	current = current->next;
+	}
 
-    return NULL;
+	return (NULL);
 }
 
