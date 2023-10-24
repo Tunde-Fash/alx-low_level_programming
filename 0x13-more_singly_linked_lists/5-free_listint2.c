75a53d3eaca827040db81374b2fd7006f3c71eb6
Tudne-Fash <tundefashicy@gmail.com>
2023-10-24 03:49:25 -0700

    5a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *current = *head;
-    listint_t *temp;
+	listint_t *current = *head;
+	listint_t *temp;
 
-    while (current != NULL)
-    {
-        temp = current->next;
-        free(current);
-        current = temp;
-    }
+	while (current != NULL)
+	{
+	temp = current->next;
+	free(current);
+	current = temp;
+	}
 
-    *head = NULL;
+	*head = NULL;
 }
