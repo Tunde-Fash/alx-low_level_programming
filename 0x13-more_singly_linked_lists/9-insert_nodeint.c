2395817fa2b7e457764dd08c471cb3fd480d2d1c
Tudne-Fash <tundefashicy@gmail.com>
2023-10-24 04:32:40 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,44 +11,43 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *current = *head;
-    unsigned int i = 0;
+	listint_t *new_node, *current = *head;
+	unsigned int i = 0;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+	return (NULL);
 
-    /* Create a new node */
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = n;
+	/* Create a new node */
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	return (NULL);
+	new_node->n = n;
 
-    /* Insert at the beginning if index is 0 */
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	/* Insert at the beginning if index is 0 */
+	if (idx == 0)
+	{
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+	}
 
-    /* Traverse the list to find the previous node to the given index */
-    while (current && i < idx - 1)
-    {
-        current = current->next;
-        i++;
-    }
+	/* Traverse the list to find the previous node to the given index */
+	while (current && i < idx - 1)
+	{
+	current = current->next;
+	i++;
+	}
 
-    /* Check if the index is out of bounds */
-    if (!current)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	/* Check if the index is out of bounds */
+	if (!current)
+	{
+	free(new_node);
+	return (NULL);
+	}
 
-    /* Insert the new node after the previous node */
-    new_node->next = current->next;
-    current->next = new_node;
+	/* Insert the new node after the previous node */
+	new_node->next = current->next;
+	current->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
-
