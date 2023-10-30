50a9f7afa29251cd015661534f0afa2be80acf69
Tudne-Fash <tundefashicy@gmail.com>
2023-10-30 12:46:17 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,42 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+	return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	return (0);
 
-    buffer = malloc(letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+	close(file_descriptor);
+	return (0);
+	}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        close(file_descriptor);
-        free(buffer);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+	close(file_descriptor);
+	free(buffer);
+	return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(file_descriptor);
-        free(buffer);
-        return (0);
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+	close(file_descriptor);
+	free(buffer);
+	return (0);
+	}
 
-    close(file_descriptor);
-    free(buffer);
+	close(file_descriptor);
+	free(buffer);
 
-    return (bytes_written);
+	return (bytes_written);
 }
