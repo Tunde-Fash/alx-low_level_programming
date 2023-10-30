ad0d93dbd9538df5eaff6e7e48bbffb934858c50
Tudne-Fash <tundefashicy@gmail.com>
2023-10-30 13:21:11 -0700

    3a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,97 +1,54 @@
 #include "main.h"
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-/**
- * _putchar - Write a character to stdout
- * @c: The character to print
- * Return: 1 (success), -1 (error)
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
+/* Helper function to print error messages */
+void print_error(const char *message, const char *filename, int exit_code) {
+    dprintf(STDERR_FILENO, message, filename);
+    exit(exit_code);
 }
 
-/**
- * cp_file - Copy file_from to file_to
- * @file_from: The source file
- * @file_to: The destination file
- * Return: 0 on success, -1 on error
- */
-int cp_file(const char *file_from, const char *file_to)
-{
-    int fd_from, fd_to, n;
+/* Copy the content of one file to another file */
+int copy_file(const char *file_from, const char *file_to) {
+    int fd_from, fd_to;
+    ssize_t bytes_read;
     char buffer[1024];
 
-    if (!file_from || !file_to)
-    {
-        dprintf(2, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
+    /* Open the source file for reading */
     fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from %s\n", file_from);
-        exit(98);
-    }
+        print_error("Error: Can't read from file %s\n", file_from, 98);
 
+    /* Open the destination file for writing, creating or truncating it */
     fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", file_to);
-        exit(99);
+        print_error("Error: Can't write to file %s\n", file_to, 99);
+
+    while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0) {
+        if (write(fd_to, buffer, bytes_read) == -1)
+            print_error("Error: Can't write to file %s\n", file_to, 99);
     }
 
-    while ((n = read(fd_from, buffer, 1024)) > 0)
-    {
-        if (write(fd_to, buffer, n) != n)
-        {
-            dprintf(2, "Error: Can't write to %s\n", file_to);
-            exit(99);
-        }
-    }
-
-    if (n == -1)
-    {
-        dprintf(2, "Error: Can't read from %s\n", file_from);
-        exit(98);
-    }
+    if (bytes_read == -1)
+        print_error("Error: Can't read from file %s\n", file_from, 98);
 
     if (close(fd_from) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
+        print_error("Error: Can't close fd %d\n", file_from, 100);
 
     if (close(fd_to) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
+        print_error("Error: Can't close fd %d\n", file_to, 100);
 
     return 0;
 }
 
-/**
- * main - Entry point
- * @argc: Argument count
- * @argv: Argument vector
- * Return: 0 on success, 97-100 on error
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: cp file_from file_to\n");
-        return (97);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+        return 97;
     }
 
-    if (cp_file(argv[1], argv[2]) == -1)
-        return (98);
-
-    return 0;
+    int result = copy_file(argv[1], argv[2);
+    return result;
 }
 
