23d8fe37b1db4796fb4ce5418f54867d25c4b914
Tudne-Fash <tundefashicy@gmail.com>
2023-10-30 13:14:32 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,32 +12,32 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len, bytes_written;
+	int fd, len, bytes_written;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+	return (-1);
 
-    if (text_content == NULL)
-        return (1);
+	if (text_content == NULL)
+	return (1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+	return (-1);
 
-    len = 0;
-    while (text_content[len] != '\0')
-        len++;
+	len = 0;
+	while (text_content[len] != '\0')
+	len++;
 
-    bytes_written = write(fd, text_content, len);
+	bytes_written = write(fd, text_content, len);
 
-    if (bytes_written == -1 || bytes_written != len)
-    {
-        close(fd);
-        return (-1);
-    }
+	if (bytes_written == -1 || bytes_written != len)
+	{
+	close(fd);
+	return (-1);
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
